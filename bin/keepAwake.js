d329db9f48b141a60549a7bec31b6412bb59976e
killianmh <killianmh@gmail.com>
2020-10-19 11:16:43 -0700

    Script only runs on weekdays for 10 hours per day

--- bin/keepAwake.js
+++ bin/keepAwake.js
@@ -1,15 +1,19 @@
 const fetch = require('node-fetch');
 
-// Setup Heroku Scheduler to run job starting at 12:00pm UTC each day
-// Since this script runs for 14 hours, this means this application will be awake 
-// from 5:00 am AZ time - 7:00 pm AZ time
+// Setup Heroku Scheduler to run job starting at 1:00pm UTC each weekday (exclude weekends)
+// using this command: node bin/keepAwake.js
 
 // Script pings listed DYNO_URL every 'interval' minutes (default 
 // is 25 minutes since heroku apps sleep after 30 minutes of non-use)
 
-// Setup Heroku Scheduler job to run using this command: node bin/keepAwake.js
+// Since this script runs for 10 hours, this means this application will be awake 
+// from 7:00 am AZ time - 5:00 pm AZ time
 
-const hours = 14
+// Max number of working days is 23 days in a month; 23 days with 10 hours each = 230 dyno hours.
+// Heroku free tier gives 1000 free hours per month. This script provides for about 3 or 4
+// free apps to be running the whole month.
+
+const hours = 10
 const start = Date.now();
 const end = start + (hours * 60 * 60 * 1000)
 
@@ -24,7 +28,10 @@ const timeouts = []
 const keepAwake = (urls, interval= 25) => {
   const milliseconds = interval * 60000
 
-  if (Date.now() < end) {
+  let todayDate = new Date(Date.now())
+  let day = todayDate.getDay()
+
+  if (day > 0 && day < 6 && Date.now() < end) {
     urls.forEach((url, i) => {
       timeouts.push(
         setTimeout(() => {
@@ -43,11 +50,16 @@ const keepAwake = (urls, interval= 25) => {
         }, milliseconds)
       )
     });
+  } else if (day === 0 || day === 6) {
+    timeouts.forEach(timeout => {
+      clearTimeout(timeout)
+    })
+    console.log('Stopping keep-awake for weekend; will restart on Monday')
   } else {
     timeouts.forEach(timeout => {
       clearTimeout(timeout)
     })
-    console.log('Stopping keep-awake until heroku scheduler starts it again')
+    console.log('Stopping keep-awake for the night; will restart tomorrow morning')
   }
 }
 
